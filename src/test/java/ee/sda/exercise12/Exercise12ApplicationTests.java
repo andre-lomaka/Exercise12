3638160457e69dbb33fdbb0d207f2bf2a58ba303
Andre Lomaka <andre7lomaka@gmail.com>
2021-06-12 13:37:47 +0300

    First commit

---
+++ src/test/java/ee/sda/exercise12/Exercise12ApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.sda.exercise12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
